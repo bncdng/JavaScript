7c2319fb6b82f1966d7f353a4b79bd383a663ab6
Bianca Dang <bncdng@gmail.com>
2024-03-18 14:19:28 +0100

    add second task

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("B" + "i" + "a" + "n" + "c" + "a");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" != 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
