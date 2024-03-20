e013dd52036c55d0581a79e6648f1a45314ffde8
Bianca Dang <bncdng@gmail.com>
2024-03-20 15:00:11 +0100

    add coding task 06

---
+++ coding-task_06/script.js
@@ -0,0 +1,59 @@
+//**************************************************
+// TODO: Implement the oddNumbers function
+
+// The function has two parameters, both are numbers.
+// Iterate over all numbers between first number and the second number.
+// Store all oddNumbers in string, seperated by a comma and return the string.
+// Allowed are only positive numbers, add a check.
+
+function oddNumbers(num1, num2) {
+  const oddNum = [];
+
+  for (let i = num1; i <= num2; i++) {
+    if (i % 2 != 0) {
+      oddNum.push(i);
+    }
+  }
+
+  return oddNum.join(",");
+}
+
+console.log(oddNumbers(0, 4));
+// result should be: 1,3
+
+console.log(oddNumbers(10, 33));
+// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+
+console.log(oddNumbers(9, 12));
+// result should be: 9,11
+
+//**************************************************
+// TODO: Implement the charCount function
+
+// The function has a first parameter for a word which is a string.
+// The function has a second parameter which contains the character to search for which is a string.
+// Check if the second parameter consists of only one character, if not, quit the function.
+// The function returns the number of times the character from the second parameter is present in the word.
+// The function should be case insensitive.
+
+function charCount(word, char) {
+  let count = 0; // Initialize a variable 'count' to store the count of occurrences of the character
+  for (let i = 0; i < word.length; i++) {
+    // Iterate through each character in the 'word' string
+
+    if (word[i] === char) {
+      // Check if the current character matches the 'char' parameter
+      count++; // If a match is found, increment the 'count' variable
+    }
+  }
+  return count; // Return the total count of occurrences of the character in the word
+}
+
+console.log(charCount("hello", "l"));
+// result should be: 2
+
+console.log(charCount("mama", "m"));
+// result should be: 2
+
+console.log(charCount("Resümee", "e"));
+// result should be: 3
